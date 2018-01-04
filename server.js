18b490fddb007a1340541a37b5fee5280b963ae1
Kanishk Srivastava <workredross@gmail.com>
2018-01-04 15:29:20 +0530

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
 
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000,function () {
-  console.log('Server up on port 3000');
-})
-//sudo netstat -lnp
-//sudo kill sudo lsof -t -i:9001`
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
